29ba03f7479888bb6e18638d3fc00bf67b4601fc
abdul razzaq <Abdul Razzaq@ps.com>
2023-09-01 16:38:41 +0500

    redux some extra

--- redux-store/src/containers/HomeContainer.js
+++ redux-store/src/containers/HomeContainer.js
@@ -1,3 +1,14 @@
+import { connect } from "react-redux";
 import Home from "../components/Home";
+import { addToCart } from "../services/Actions/action";
 
-export default Home;
+const mapDispatchToProps = (dispatch) => ({
+  addCartHandler: (data) => dispatch(addToCart(data)),
+});
+
+const mapStateToProps = (state) => ({
+  cardData: state,
+});
+
+export default connect(mapDispatchToProps, mapStateToProps)(Home);
+// export default Home;
--- redux-store/src/index.js
+++ redux-store/src/index.js
@@ -4,13 +4,19 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+//
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducers from "./services/Reducers/index";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const store = createStore(rootReducers);
+// console.log("Store", store);
 root.render(
-  // <React.StrictMode>
-  <App />,
-  {
-    /* </React.StrictMode> */
-  }
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  {}
 );
 
 // If you want to start measuring performance in your app, pass a function
